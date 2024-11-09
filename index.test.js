a3df21b0a647da815296d9fdb5e0774370b0c225
Sakis T. <106918030+AthTop@users.noreply.github.com>
2024-11-09 19:05:42 +0200

    Implement capitalize function and tests for it

--- index.test.js
+++ index.test.js
@@ -1,4 +1,25 @@
-it("Test adds 1 + 2", () => {
-    expect(1 + 2).toBe(3);
-  });
-  
+import { capitalize } from "./index.js";
+
+it("capitalize string", () => {
+  expect(capitalize("abc")).toBe("ABC");
+});
+
+it("capitalize second string", () => {
+  expect(capitalize("def")).toBe("DEF");
+});
+
+it("capitalize string with space", () => {
+  expect(capitalize("string with space")).toBe("STRING WITH SPACE");
+});
+
+it("capitalize mixed upper/lower", () => {
+  expect(capitalize("UpPeRLoWeR")).toBe("UPPERLOWER");
+});
+
+it("capitalize with numbers", () => {
+  expect(capitalize("abc123")).toBe("ABC123");
+});
+
+it("capitalize with special characters", () => {
+  expect(capitalize("abc#@!")).toBe("ABC#@!");
+});
